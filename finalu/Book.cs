9c939dcea62a0172247435e6812c8866ca9cfca5
Nino kordeli <nino.kordeli.1@btu.edu.ge>
2024-02-22 17:52:15 +0400

    Add project files.

---
+++ finalu/Book.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalu
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ReleaseDate { get; set; }
+
+        public Book(string title, string author, string releaseDate)
+        {
+            Title = title;
+            Author = author;
+            ReleaseDate = releaseDate;
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {Title}\nAuthor: {Author}\nPublication Year: {ReleaseDate}";
+        }
+    }
+
+    public class bookManager
+    {
+        private List<Book> books = new List<Book>();
+
+        public void AddBook(string title, string author, string releaseDate)
+        {
+            Book newBook = new Book(title, author, releaseDate);
+            books.Add(newBook);
+        }
+        public void ShowBooks()
+        {
+            Console.WriteLine("Book List");
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+        public Book SearchByBook(string title)
+        {
+            foreach (var book in books)
+            {
+                if (book.Title == title)
+                {
+                    return book;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ finalu/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the operator");
+            string calc = Console.ReadLine();
+            switch (calc)
+            {
+                case "+":
+                    Console.WriteLine(num+num2); 
+                    break;
+                case "-": 
+                    Console.WriteLine(num-num2);
+                    break;
+                case "/":
+                    Console.WriteLine(num / num2);
+                    break;
+                case "*":
+                    Console.WriteLine(num * num2);
+                    break;
+                case "%":
+                    Console.WriteLine(num % num2);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    break;
+            }
+
+            Random random = new Random();
+            int Gnum = random.Next(1, 11);
+            int guess;
+            int attempts = 0;
+            Console.WriteLine("Number Guessing Game!");
+            do
+            {
+                Console.WriteLine("Enter a number from one to ten");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (guess < Gnum)
+                    Console.WriteLine("Number too low, try again!");
+                else if (guess > Gnum)
+                    Console.WriteLine("Number too High, try again!");
+            }
+            while (guess!= Gnum);
+            Console.WriteLine($"You guessed the number {Gnum} correctly");
+        }
+    }
+}
