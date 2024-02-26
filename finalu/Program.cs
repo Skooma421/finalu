996f825ebce739cdf92ca77567dfd2a2822a924d
Nino kordeli <nino.kordeli.1@btu.edu.ge>
2024-02-26 21:01:51 +0400

    ATM added.

--- finalu/Program.cs
+++ finalu/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,8 @@ namespace finalu
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Enter your number:");
+        {   //martivi kalkulatori switchit
+            Console.WriteLine("Enter your number:");//users sheyavs monacemebi
             int num = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter your second number");
             int num2 = int.Parse(Console.ReadLine());
@@ -38,13 +39,13 @@ namespace finalu
                     Console.WriteLine("Invalid operator");
                     break;
             }
-
-            Random random = new Random();
+            //ricxvis gamosacnobi tamashi
+            Random random = new Random();//viyenebt rands rom davagenerirot ertidan atis chatvlit ricxvebi
             int Gnum = random.Next(1, 11);
             int guess;
             int attempts = 0;
             Console.WriteLine("Number Guessing Game!");
-            do
+            do//loopi
             {
                 Console.WriteLine("Enter a number from one to ten");
                 guess = int.Parse(Console.ReadLine());
@@ -57,6 +58,86 @@ namespace finalu
             }
             while (guess!= Gnum);
             Console.WriteLine($"You guessed the number {Gnum} correctly");
+
+            //ATM aplikacia
+            String directoryPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\ATM";
+            Directory.CreateDirectory(directoryPath);
+
+            AddAccount("123456", "Nino", 522, directoryPath);
+
+            Console.Write("Enter your account number: ");
+            String accountNum = Console.ReadLine();
+
+            string accountFile = Path.Combine(directoryPath, $"{accountNum}.txt");
+            if (!File.Exists(accountFile))
+            {
+                Console.WriteLine("Account not found.");
+                return;
+            }
+            Console.WriteLine("1.Check balance");
+            Console.WriteLine("2.Deposit");
+            Console.WriteLine("3.Withdraw");
+            Console.WriteLine("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    CheckBalance(accountFile); 
+                    break;
+                case "2":       
+                    Deposit(accountFile); 
+                    break;
+                case "3":
+                    Withdraw(accountFile); 
+                    break;
+                    default: Console.WriteLine("Invalid choice.");
+                    break;
+            }
+        }
+        static void AddAccount(string accountNumber, string accountName, decimal balance, string directoryPath)
+        {
+            string accountFile = Path.Combine(directoryPath, $"{accountNumber}.txt");
+
+            using (StreamWriter writer = new StreamWriter(accountFile))
+            {
+                writer.WriteLine(accountNumber);
+                writer.WriteLine(balance);
+            }
+        }
+        static void CheckBalance (string accountFile)
+        {
+            string balance = File.ReadAllText(accountFile);
+            Console.WriteLine($"Your current balance: {balance}");            
+        }
+        static void Deposit (string accountFile) 
+        {
+            Console.Write("Enter Deposit amount: ");
+            string depositAmount = Console.ReadLine();
+
+            decimal currentBal= decimal.Parse(File.ReadAllText(accountFile));
+            decimal newBal = currentBal + decimal.Parse(depositAmount);//aq vumatebs arsebul tanxas axal depozits
+            
+            File.WriteAllText(accountFile,newBal.ToString());
+            Console.WriteLine("Deposit finished.");
+        }
+        static void Withdraw(string accountFile)
+        {
+            Console.WriteLine("Enter widhtrawl amount: ");
+            string withAmount = Console.ReadLine(); 
+
+            decimal currentBal = decimal.Parse(File.ReadAllText(accountFile));
+
+            decimal withdrawl = decimal.Parse(withAmount);
+            if(withdrawl > currentBal)
+            {
+                Console.WriteLine("Not enought balance to withdraw");
+                return;
+            }
+            decimal newBal = currentBal - withdrawl;
+
+            File.WriteAllText (accountFile,newBal.ToString());
+            Console.WriteLine("Withdrawl finished.");
         }
     }
 }
